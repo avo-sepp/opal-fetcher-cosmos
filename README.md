5afb82f27cd610be0d16d866a9a3c4ddb1ed41fb
Avo Sepp <ashahverdian@nba.com>
2023-01-31 15:00:03 -0500

    skeleton file structure

---
+++ README.md
@@ -0,0 +1,12 @@
+<p  align="center">
+ <img src="https://i.ibb.co/BGVBmMK/opal.png" height=170 alt="opal" border="0" />
+</p>
+<h2 align="center">
+OPAL Fetcher for CosmosDB
+</h2>
+
+[Check out OPAL main repo here.](https://github.com/permitio/opal)
+
+# Work in Progress
+
+The purpose of this is to build a OPAL fetcher for CosmosDB. See original fetcher here: https://github.com/permitio/opal-fetcher-postgres
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='opal-fetcher-cosmosdb',
+    version='0.0.1',
+    author='Avo Sepp',
+    author_email="",
+    description="An OPAL fetch provider to bring authorization state from CosmosDB.",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/avo-sepp/opal-fetcher-cosmosdb",
+    packages=find_packages(),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'opal-common>=0.1.11',
+        'azure-cosmos',
+        'pydantic',
+        'tenacity',
+        'click'
+    ],
+)
